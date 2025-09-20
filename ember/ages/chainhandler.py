3b4cc15082dc473cfe4c4c19e1a31debafab8ad2
Stefan Arseneau <arseneausm@gmail.com>
2025-09-20 16:34:30 -0400

    removed unnecessary environment variabel

--- ember/ages/chainhandler.py
+++ ember/ages/chainhandler.py
@@ -2,10 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import tqdm
-
 import os, sys, glob, re
-agedir = os.environ['WDAGE_PATH']
-data_dir = os.path.join(agedir, "data")
 
 from . import ageinterp
 
